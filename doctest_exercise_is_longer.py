99f38b031580c3febd8df0b517c7ebf6daf47b93
Carson.O <carcango@gmail.com>
2022-02-02 15:43:36 -0800

    Added a solution to determine if first list is longer than the second list in doctest_exercise_is_longer.py. Added additional doctests to function

--- doctest_exercise_is_longer.py
+++ doctest_exercise_is_longer.py
@@ -1,6 +1,6 @@
 """
-Your name:
-Your student number:
+Your name: Carson Olafson
+Your student number: A01221217
 """
 
 
@@ -15,11 +15,28 @@ def is_longer(first_list, second_list):
     :postcondition: second_list is not changed
     :return: True if first_list is longer than second_list, else False
 
+    Test 1: Default test, returns true if first list is longer than the second. It did.
+
+    Test 2: Default test, tested if it would return false for lists with elements split up. It did.
+
+    Test 3. Default test, tested to see if it works to compare integers and strings. It did.
+
+    Test 4. Tested to see if it values floats as a single element. It did
+
+    Test 5. Tested to see if it would value elements with exponents as a single element. It did
+
     >>> is_longer([1, 2, 3], [4, 5])
     True
     >>> is_longer(['abcdef'], ['ab', 'cd', 'ef'])
     False
     >>> is_longer(['a', 'b', 'c'], [1, 2, 3])
     False
+    >>> is_longer([1, 2, 3], [4.5, 5.0])
+    True
+    >>> is_longer([1, 2, 3], [4^9, -5])
+    True
     """
-
+    if len(first_list) > len(second_list):
+        return True
+    else:
+        return False
