c283d8c03e6039ab8fea1cd10962070a2dd300be
Carson.O <carcango@gmail.com>
2022-02-02 16:38:47 -0800

    Added completed function to line 37 - 39 of doctest_exercise_total_length.py. Also added 2 new doctests and corresponding comments in docstring

--- doctest_exercise_total_length.py
+++ doctest_exercise_total_length.py
@@ -1,6 +1,6 @@
 """
-Your name:
-Your student number:
+Your name: Carson Olafson
+Your student number: A01221217
 """
 
 
@@ -15,10 +15,24 @@ def total_length(first_string, second_string):
     :postcondition: correctly sums the lengths of the strings
     :return: the sum of the lengths, an integer
 
+    Test 1 - 3: Default tests, test if length counter works with two strings or blank spaces. It did
+
+    Test 4. Test if numbers work as valid string. It did
+
+    Test 5. Test if floats work as valid string, and if it counted the period and the floating number as individual
+            elements. It did
+
+
     >>> total_length('yes', 'no')
     5
     >>> total_length('yes', '')
     3
     >>> total_length('YES!!!!', 'Noooooo')
     14
+    >>> total_length('1234567890', 'no')
+    12
+    >>> total_length('1.0', 'no')
+    5
     """
+    length_total = len(first_string) + len(second_string)
+    return length_total
